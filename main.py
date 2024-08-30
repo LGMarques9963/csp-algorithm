9d4f668770e962a8e4f535b00bceb55b743b73ab
= <=>
2024-08-29 22:04:04 -0300

    restricoes

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ def ler_grid():
         a = f.read()
         return a.split("\n")
 
+def ler_palavras():
+    with open('lista_palavras.txt') as f:
+        a = f.read()
+        return a.split("\n")
+
 print(ler_grid())
 a = ler_grid()
 print(type(a))
+b = a[0].find(".")
+print(b)
+print(a[0][b])
+
+palavras = ler_palavras()
+print(palavras)
